4333ad0b17cce7acff4a22ad87a9f35469edd350
Andrew <shifted6@gmail.com>
2020-03-16 02:14:58 -0700

    First code challenge problem in C# complete, array max result.

---
+++ DSA Practice/DSAPractice/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ArrayMaxResult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 5;
+            int[] inputArray = new int[length];
+            for(int i=0; i<length; i++)
+            {
+                Console.Write($"Enter a number between 1 and 10 ({i} entered): ");
+                inputArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Write("Select a value to see the score of that value: ");       
+            int selectedVal = Convert.ToInt32(Console.ReadLine());
+            int score = CalculateScore(inputArray, selectedVal);
+            Console.WriteLine($"Score: {score}");
+        }
+        private static int CalculateScore(int[] array, int val)
+        {
+            int score = 0;
+            foreach(int arrayVal in array)
+            {
+                if(arrayVal == val)
+                {
+                    score += arrayVal;
+                }
+            }
+            return score;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # CodeChallenge
+
+# Code Challenge 1: DSA Practice
+
+## Problem 1: Array Max Result
+### Problem Statement
+Given an array select a number that exists, and output the computated "score". The method you create should take in both an array of integers and the integer the user selected.
+
+Create a Console application that requests 5 numbers between 1-10 from the user. Output the array to the console and ask the user to select a number. After the selection, output the "score" of the number chosen.
+
+### Selection and Scoring Algorithm
+You can select any number from the list, however your scoring will be depend on the frequency of that number in the list. E.g for [2,2,3,5,4] if you pick 2 your score will be 4 (2 * 2) but if you pick 5 your score will be 5 (5 * 1)
+
+### Input Format
+An array of integers, and the number chosen.
+
+### Sample Input
+[6,4,4,1,3] , 4
+
+### Sample Output
+8
+
+https://codefellows.slack.com/files/US18RKRNV/FV64B1HGR/arraymaxresult_success.png
